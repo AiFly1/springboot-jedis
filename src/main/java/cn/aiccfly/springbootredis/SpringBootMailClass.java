b5c3609c7fd8d2fec602c8d67ea967bb20a150aa
aiccfly <aiccfly@163.com>
2020-05-08 22:45:36 +0800

    Merge branch m

--- src/main/java/cn/aiccfly/springbootredis/SpringBootMailClass.java
+++ src/main/java/cn/aiccfly/springbootredis/SpringBootMailClass.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMailClass {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMailClass.class,args);
-
+        SpringApplication.run(SpringBootMailClass.class,args);
 
         //这是master分支
     }
